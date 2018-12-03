f880d3b5e884f22c3747a911568dd9735823604a
Igor <sowinski.igor@gmail.com>
2018-12-03 12:20:01 +0100

    1# added check summ service

--- checksumm-service/src/main/java/com/wildBirds/checksummservice/domain/port/InnNumberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wildBirds.checksummservice.domain.port;
-
-import com.wildBirds.checksummservice.domain.model.InnNumber;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InnNumberRepository extends JpaRepository<InnNumber, Integer> {
-
-
-}
