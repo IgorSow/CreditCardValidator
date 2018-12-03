b37bc84a758385350b2416529ca4fd981797a1ea
Igor <sowinski.igor@gmail.com>
2018-12-03 14:43:59 +0100

    1# create Card Validator

---
+++ card-validator/src/main/java/com/wildBirds/cardvalidator/CardValidatorApplication.java
@@ -0,0 +1,12 @@
+package com.wildBirds.cardvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardValidatorApplication.class, args);
+	}
+}
